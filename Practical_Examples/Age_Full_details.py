fe6e46d8563ea7029a3f950c77921a6457b9cf4f
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2025-02-20 18:59:38 +0200

    Age_Full_details.py

---
+++ Practical_Examples/Age_Full_details.py
@@ -0,0 +1,9 @@
+# Your age full details
+age = int(input("Enter your age : ").strip())
+months = age * 12
+weeks = age * 52
+days = age * 365
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+print(f"You lived for : \n {months:,} months. \n {weeks:,} weeks. \n {days:,} days. \n {hours:,} hours. \n {minutes:,} minutes. \n {seconds:,} seconds.")
