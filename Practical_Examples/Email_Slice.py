cc394f69f17dbf50f278880bbbbb4bcc4518bdd0
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2025-02-20 18:46:06 +0200

    Email_Slice.py

---
+++ Practical_Examples/Email_Slice.py
@@ -0,0 +1,11 @@
+# Email slice 
+thename  = input("Enter your name : ").strip().capitalize()
+email = input("Enter your email : ").strip().capitalize()
+
+if email.find("@") != -1:                   # if email.find("@") != -1:  is same as if "@" in email
+    theusername = email[:email.index("@")]
+    thewebsite = email[email.index("@"):]
+    print(f" Hello {thename}  \n email is {email}")
+    print(f" Your user name is {theusername} \n Your website is {thewebsite}")
+else :
+    print("Invalid email")
